37e8a5654873a14265e6e254e300555c40c5e7dc
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-10-02 13:20:49 -0300

    Fix formatting

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -5,25 +5,23 @@ import beeAnimation from '../../assets/lotties/bee-animation.json'
 
 
 const Loader = styled.div`
-pointer-events: none;
-display: flex;
-align-items: center;
-justify-content: center;
-height: ${props => props.fill ? '100vh': '180px'};
-width: 100%;
+  pointer-events: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: ${props => props.fill ? '100vh': '180px'};
+  width: 100%;
 `
 
 const LocalLoader = ({ fill }) => {
   const defaultOptions = {
-    loop: true,
-    autoplay: true,
     animationData: beeAnimation,
   };
 
 return (
  <Loader fill={fill}>
     <Lottie 
-    options={defaultOptions}
+      options={defaultOptions}
       height={fill ? 100 : 84}
       width={fill ? 100 : 84}
     />
