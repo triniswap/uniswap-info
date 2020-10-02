777911efa9f0684e0e870532a32c60f69c74523e
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-10-02 13:01:48 -0300

    Replace loader

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -1,42 +1,34 @@
 import React from 'react'
-import styled, { css, keyframes } from 'styled-components'
-import { useDarkModeManager } from '../../contexts/LocalStorage'
+import Lottie from 'react-lottie'
+import styled from 'styled-components'
+import beeAnimation from '../../assets/lotties/bee-animation.json'
 
-const rotate = keyframes`
-  0% { transform: scale(1); }
-  60% { transform: scale(1.1); }
-  100% { transform: scale(1); }
-`
 
 const Loader = styled.div`
-  pointer-events: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-  animation: ${rotate} 800ms linear infinite;
-  & > * {
-    width: 72px;
-  }
-  ${props =>
-    props.fill && !props.height
-      ? css`
-          height: 100vh;
-        `
-      : css`
-          height: 180px;
-        `}
+pointer-events: none;
+display: flex;
+align-items: center;
+justify-content: center;
+height: ${props => props.fill ? '100vh': '180px'};
+width: 100%;
 `
 
 const LocalLoader = ({ fill }) => {
-  const [darkMode] = useDarkModeManager()
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: beeAnimation,
+  };
 
-  return (
-    <Loader fill={fill}>
-      <img src={require(darkMode ? '../../assets/logo_white.svg' : '../../assets/logo.svg')} alt="loading-icon" />
-    </Loader>
-  )
+return (
+ <Loader fill={fill}>
+    <Lottie 
+    options={defaultOptions}
+      height={fill ? 100 : 84}
+      width={fill ? 100 : 84}
+    />
+  </Loader>
+);
 }
 
 export default LocalLoader
